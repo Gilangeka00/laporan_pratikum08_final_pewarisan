430e00b235565700114977cc1ec83f3e0c40b4bc
gilang <gilangekayanda@students.amikom.ac.id>
2023-06-05 21:24:07 +0700

    first commit

---
+++ laporan_pratikum08_final_pewarisan/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lanjutan_Inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person Person1 = new Person();
+            Person1.Name = "Meysi";
+            Person1.Age = 19;
+            Person1.InfoPerson();
+
+            Karyawan Karyawan1 = new Karyawan();
+            Karyawan1.Name = "Gilang";
+            Karyawan1.Age = 18;
+            Karyawan1.InfoPerson();
+            Karyawan1.InfoPerson("Dosen", "AA2002200400123AC0XB100");
+
+            Console.ReadLine();
+        }
+    }
+}
